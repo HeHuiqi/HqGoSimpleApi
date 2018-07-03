a694db0b78fb029c9da4878f54c419dc42435f2d
hhq <huiqi.he@solarsoft.org>
2018-07-03 18:04:28 +0800

    change

--- cgo/RouterHandler.go
+++ cgo/RouterHandler.go
@@ -15,13 +15,17 @@ type RouterHandler struct {
 var mux = make(map[string]func(http.ResponseWriter,*http.Request))
 
 func (p *RouterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
+	fmt.Println("mux：",mux)
+
+	fmt.Println("r.URL.Path：",r.URL.Path)
 	if fun, ok := mux[r.URL.Path]; ok {
 		fun(w, r)
 		return
 	}
 	//静态资源
 	if strings.HasPrefix(r.URL.Path,constant.STATIC_BAES_PATH){
+		fmt.Println("静态资源：",r.URL.Path)
+
 		if fun, ok := mux[constant.STATIC_BAES_PATH]; ok {
 			fun(w, r)
 			return
